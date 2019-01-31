e33885d58aa9d6cbd382833be091f64758904fec
TheNeoCoder <the.neocoder@gmail.com>
2019-01-30 21:33:45 -0500

    reset the .gitignore

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Layout from "../comps/Layout.js";
+//import Posts from "../comps/Posts.js";
+import ReactSVG from "react-svg";
+
+const Index = () => (
+    <Layout>
+    </Layout>
+);
+
+export default Index;
